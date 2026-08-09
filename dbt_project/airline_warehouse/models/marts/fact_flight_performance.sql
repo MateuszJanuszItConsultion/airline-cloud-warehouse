4b2bd1706ceaf96cd4cfbb5b76a3a3cd62d552cf
Mateusz Janusz <januszmateusz@gmail.com>
2026-08-09 12:31:19 +0200

    feat: add fact_flight_performance mart

---
+++ dbt_project/airline_warehouse/models/marts/fact_flight_performance.sql
@@ -0,0 +1,13 @@
+
+{{ config(materialized='table') }}
+
+select
+    flight_date,
+    carrier_code,
+    flight_number,
+    origin_airport,
+    dest_airport,
+    dep_delay_minutes,
+    arr_delay_minutes,
+    is_cancelled
+from {{ ref('stg_flights') }}
