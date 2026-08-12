0921cb784945f010451df284f9d4803fcb5ff20c
Mateusz Janusz <januszmateusz@gmail.com>
2026-08-12 19:57:19 +0200

    refactor: move deduplication and enrichment to Silver layer, add delay_category/season/utilization_level; add on_schema_change safeguard to incremental facts

--- dbt_project/airline_warehouse/models/staging/stg_weather.sql
+++ dbt_project/airline_warehouse/models/staging/stg_weather.sql
@@ -1,4 +1,4 @@
-{{ config(materialized='view') }}
+{{ config(materialized='table') }}
 
 with source as (
     select * from {{ source('bronze', 'weather_raw') }}
@@ -16,6 +16,37 @@ renamed as (
         _ingested_at,
         _source_file
     from source
+),
+
+deduplicated as (
+    select
+        *,
+        row_number() over (
+            partition by observation_date, airport_code
+            order by _ingested_at desc
+        ) as _row_num
+    from renamed
+),
+
+enriched as (
+    select
+        observation_date,
+        airport_code,
+        avg_temp_c,
+        precipitation_mm,
+        avg_wind_speed_kmh,
+        visibility_km,
+        has_severe_weather,
+        case
+            when month(observation_date) in (12, 1, 2) then 'Winter'
+            when month(observation_date) in (3, 4, 5) then 'Spring'
+            when month(observation_date) in (6, 7, 8) then 'Summer'
+            else 'Fall'
+        end as season,
+        _ingested_at,
+        _source_file
+    from deduplicated
+    where _row_num = 1
 )
 
-select * from renamed
+select * from enriched
